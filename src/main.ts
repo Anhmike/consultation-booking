71d1978e116d5cabcb4e419609d5655e583cd10a
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-05-08 14:06:34 +0100

    initialise angular material2 and add main toolbar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {ConsultationBookingAppComponent, environment} from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(ConsultationBookingAppComponent);
+bootstrap(ConsultationBookingAppComponent, [HTTP_PROVIDERS]);
